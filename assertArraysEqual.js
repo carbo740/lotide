441a00ea4267a5b18cf665970fabeb2540748a2d
Charles Arboleda <charlesarboleda_@hotmail.com>
2022-10-20 23:29:06 +0000

    eslint assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,14 @@
-const eqArrays = function(array1, array2){
+const eqArrays = function(array1, array2) {
 
-  for(let i = 0; i < array1.length; i++){
-    if(array1[i] !== array2[i]){
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return false;
     }
   }
   return true;
 };
 
-const assertArraysEqual = function (array1, array2){
+const assertArraysEqual = function(array1, array2){
 
   if (eqArrays(array1,array2)) {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
