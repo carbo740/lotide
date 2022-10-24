2f23d047f7d2cb2b57879d09557cb98d65bba677
Charles Arboleda <charlesarboleda_@hotmail.com>
2022-10-24 23:56:38 +0000

    index.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,8 +4,10 @@ const assertArraysEqual = function(array1, array2){
 
   if (eqArrays(array1,array2)) {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+    return true;
   } else {
     console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+    return false;
   }
 };
 
