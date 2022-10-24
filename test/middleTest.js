2706adb5a68b940970961b396551f5d92ae476e7
Charles Arboleda <charlesarboleda_@hotmail.com>
2022-10-24 23:48:33 +0000

    Added assert.deepEqual for comparing Arrays

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,19 +1,17 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-
 const assert = require('chai').assert;
 
 describe("#middle", () => {
   it("should return true", () => {
-    assert.strictEqual(assertArraysEqual(middle([1,2,3,4,5]), [3]), true);
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
   });
 
   it("should return false", () => {
-    assert.strictEqual(assertArraysEqual(middle([2,3,4,5]), [2,4]), false);
+    assert.deepEqual(middle([2,3,4,5]), [3,4]);
   });
 
   it("should return true", () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2]), []), true);
+    assert.deepEqual(middle([1, 2]), []);
   });
 
 });
