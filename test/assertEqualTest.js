4efe1fad0aa71353b8d5631b00a362e63faba8fc
Charles Arboleda <charlesarboleda_@hotmail.com>
2022-10-24 21:55:33 +0000

    Refactoring assertEqual && head for tests

---
+++ test/assertEqualTest.js
@@ -0,0 +1,4 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual(1 , 5);
+assertEqual("Hello World", "Hello World");
---
+++ test/headTest.js
@@ -0,0 +1,5 @@
+const head = require('../head');
+const assertEqual = require('../assertEqual');
+
+assertEqual(head([1, 2, 3]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
